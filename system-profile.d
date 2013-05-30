32f0fd390510a51a84b7bd70f0d59ce07aa70acf
Rob <me@robhaswell.co.uk>
2013-05-30 14:13:41 +0100

    system-profile.d

---
+++ system-profile.d
@@ -0,0 +1,13 @@
+#!/usr/sbin/dtrace -s
+
+#pragma D option quiet
+#pragma D option defaultargs
+#pragma D option bufsize=200m
+#pragma D option dynvarsize=256m
+#pragma D option cleanrate=5000hz
+
+profile:::profile-4001
+{
+    @stacks[pid, tid, execname, stack()] = count();
+}
+
